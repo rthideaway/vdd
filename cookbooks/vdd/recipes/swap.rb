29dbc1fdaa18d88a1b72f1033b2f2de250f2b21c
Dan James <danj@deeson.co.uk>
2017-02-03 15:21:22 +0000

    I picked a size for swap. the parent disk is only 10GB so memory sizes much above 2gb fill the disk up pretty quickly.

--- cookbooks/vdd/recipes/swap.rb
+++ cookbooks/vdd/recipes/swap.rb
@@ -1,5 +1,14 @@
 # Swap should generally be double your memory allocation.
-swap_size = (node["vm"]["memory"].to_i) * 2
+swap_size = 2048
+
+# It's probably not related to swap, but it is a really early set up thing.
+# See https://gist.github.com/rmarchei/4f63b6682766721e178eec9722dc7368
+execute 'fix-ubuntu-disk' do
+  command 'udevadm trigger --action=add --subsystem-match=scsi'
+  action :run
+end
+
+# echo 180 > /sys/block/sda/device/timeout
 
 script 'create swapfile' do
   interpreter 'bash'
