99f119f5e52a0bb5f447b7c2e6967e935823409f
Dan James <danj@deeson.co.uk>
2017-02-16 09:56:07 +0000

    Setting up xdebug.

--- cookbooks/vdd/recipes/php.rb
+++ cookbooks/vdd/recipes/php.rb
@@ -24,53 +24,26 @@ template "/etc/php/7.0/mods-available/vdd_php.ini" do
   notifies :restart, "service[php7.0-fpm]", :delayed
 end
 
-# template "/etc/php5/mods-available/xdebug.ini" do
-#   source "php/xdebug.ini.erb"
-#   mode "0644"
-#   notifies :restart, "service[apache2]", :delayed
-#   notifies :restart, "service[php5-fpm]", :delayed
-# end
-# template "/etc/php5/mods-available/xdebug-cli.ini" do
-#   source "php/xdebug-cli.ini.erb"
-#   mode "0644"
-#   notifies :restart, "service[apache2]", :delayed
-#   notifies :restart, "service[php5-fpm]", :delayed
-# end
+execute 'phpenmod' do
+  command "phpenmod vdd_php"
+end
 
-phptypes = [
-  "apache2",
-  "cli",
-  "fpm"
-]
+template "/etc/php/7.0/mods-available/xdebug_http.ini" do
+  source "php/xdebug.ini.erb"
+  mode "0644"
+  notifies :restart, "service[php7.0-fpm]", :delayed
+end
 
-modules = [
-  "vdd_php",
-  "uploadprogress",
-  "pdo_mysql",
-  "mongo",
-  "memcache",
-  "sqlite3",
-  "mcrypt",
-  "imagick",
-  "xhprof"
-]
+execute 'phpenmod' do
+  command "phpenmod xdebug_http -s fpm"
+end
 
-# modules.each do |mod|
-#   bash "enable_php_module_#{mod}" do
-#     user "root"
-#     code <<-EOH
-#     php5enmod #{mod}
-#     EOH
-#     only_if { File.exists?("/etc/php5/mods-available/#{mod}.ini") }
-#   end
-# end
+template "/etc/php/7.0/mods-available/xdebug_cli.ini" do
+  source "php/xdebug-cli.ini.erb"
+  mode "0644"
+end
 
-# We want a different xdebug config for cli than for everything else
-# so do this using link rather than php5enmod.
-# link '/etc/php5/fpm/conf.d/20-xdebug.ini' do
-#   to '/etc/php5/mods-available/xdebug.ini'
-# end
-
-# link '/etc/php5/cli/conf.d/20-xdebug.ini' do
-#   to '/etc/php5/mods-available/xdebug-cli.ini'
+# Leaving this off as I suspect it breaks things.
+# execute 'phpenmod' do
+#   command "phpenmod xdebug_http -s cli"
 # end
