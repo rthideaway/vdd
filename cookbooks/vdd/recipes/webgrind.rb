640e627c7eb14f15be7f5b67ac35087e4a040455
John Ennew <johne@deeson.co.uk>
2017-02-24 21:28:31 +0000

    Fixes #40 - removed unuesed recipes

--- cookbooks/vdd/recipes/webgrind.rb
+++
@@ -1,9 +0,0 @@
-git "/var/www/webgrind" do
-  repository "https://github.com/jokkedk/webgrind.git"
-  reference "master"
-  action :sync
-end
-
-link "/usr/local/bin/dot" do
-  to "/usr/bin/dot"
-end
