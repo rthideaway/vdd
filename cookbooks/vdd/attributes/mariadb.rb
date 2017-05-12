5ea3a1d1233cc2257b280e228d58df96e7b75603
Radoslav Terezka <radoslavterezka@gmail.com>
2017-05-12 16:03:36 +0200

    DCD-9 shutdown and restart mariadb to apply settings (like remote root access)

--- cookbooks/vdd/attributes/mariadb.rb
+++ cookbooks/vdd/attributes/mariadb.rb
@@ -1,2 +1,3 @@
+default['mariadb']['server_root_password'] = 'root'
 default['mariadb']['forbid_remote_root'] = false
 default['mariadb']['mysqld']['bind_address'] = '0.0.0.0'
--- cookbooks/vdd/recipes/mariadb.rb
+++ cookbooks/vdd/recipes/mariadb.rb
@@ -1,2 +1,17 @@
 # Install MariaDB
 include_recipe 'mariadb'
+
+# Restart mysql service
+execute "shutdown_mysql" do
+  user "root"
+  group "root"
+  command <<-EOF
+   /usr/bin/mysqladmin -u root -p#{node['mariadb']['server_root_password']} shutdown
+  EOF
+  notifies :restart, 'service[restart_mysql]', :immediately
+end
+
+service "restart_mysql" do
+  service_name 'mysql'
+  action :nothing
+end
